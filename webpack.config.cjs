def7acc5b1047360e72674da4c534ac5ddfe7501
Dex <sheepareawesome777@gmail.com>
2023-05-14 12:25:22 -0400

    more chat jippity

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -9,47 +9,46 @@ const dirs = glob.sync("sparks/*/");
 
 dirs.forEach((dir) => {
 	// Get the directory name from the path
-    const dirName = path.basename(path.dirname(dir));
-    // Define the entry path for the current directory
-    const entryPath = `./${dir}/main.ts`;
-    // Define the output path for the current directory
-    const outputPath = `./dist/${dirName}/`;
+	const dirName = path.basename(path.dirname(dir));
+	// Define the entry path for the current directory
+	const entryPath = `./${dir}/main.ts`;
+	// Define the output path for the current directory
+	const outputPath = `./dist/${dirName}/`;
 
-    entries[dirName] = {
-        entry: entryPath,
-        output: {
-            filename: "main.js",
-            path: path.resolve(__dirname, outputPath)
-        }
-    };
+	entries[dirName] = {
+		entry: entryPath,
+		output: {
+			filename: "main.js",
+			path: path.resolve(__dirname, outputPath)
+		}
+	};
 });
 
+module.exports = Object.values(entries).map((entry) => ({
+	mode: "production",
+	entry: entry.entry,
+	output: entry.output,
 
-module.exports = {
-    mode: "production",
-    entry: entries.entry,
-    output: entries.output,
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/
+			}, {
+				test: /\.scss$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+			}
+		]
+	},
 
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/
-            }, {
-                test: /\.scss$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-            }
-        ]
-    },
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"]
+	},
 
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"]
-    },
-
-    plugins: [
+	plugins: [
 		new MiniCssExtractPlugin(
-            {filename: "[name]/main.css"}
-        )
+			{filename: "[name]/main.css"}
+		)
 	]
-};
+}));
