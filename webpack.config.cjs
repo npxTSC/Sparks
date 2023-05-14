38b67969c208c60c3ae376bd97730e4d79763d56
Dex <sheepareawesome777@gmail.com>
2023-05-14 13:46:30 -0400

    Update `libdx` (dumbass error fixed) + Add `theme.scss`

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,42 +1,71 @@
-const path = require('path');
-const glob = require('glob');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+"use strict";
+const path					= require("path");
+const CleanTerminalPlugin	= require("clean-terminal-webpack-plugin");
+const CopyPlugin			= require("copy-webpack-plugin");
+
+const cd = __dirname;
 
 module.exports = {
-	entry: () => {
-		const entries = {};
-		glob.sync('./sparks/*/main.ts').forEach((file) => {
-			const folder = path.dirname(file);
-			entries[folder] = file;
-		});
-		return entries;
+	mode: "production",
+	entry: {
+		"sparks/sparkwave":			cd + "/sparks/sparkwave/main.ts",
+		"sparks/sass-sparkwave":	cd + "/sparks/sparkwave/sparkwave.scss",
+		
+		"sparks/wordle":			cd + "/sparks/wordle/main.ts",
+		"sparks/sass-wordle":		cd + "/sparks/wordle/wordle.scss",
+		
+		"sparks/sudoku":			cd + "/sparks/sudoku/main.ts",
+		"sparks/sass-sudoku":		cd + "/sparks/sudoku/sudoku.scss",
 	},
 	
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: '[name]/[name].js'
-	},
-
-	resolve: {
-		extensions: ['.ts', '.js']
-	},
-
 	module: {
 		rules: [
 			{
-				test: /\.ts$/,
+				test: /\.js$/,
+				enforce: "pre",
+				use: ["source-map-loader"],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							outputPath: "css/",
+							name: "[path][name].min.css",
+							context: "src/css/",
+						}
+					},
+					"sass-loader"
+				]
+			},
+			{
+				test: /\.tsx?$/,
+				use: [{
+					loader: "ts-loader",
+					options: {
+						configFile: "tsconfig.webpack.json"
+					}
+				}],
 				exclude: /node_modules/,
-				use: 'ts-loader'
-			}, {
-				test: /\.scss$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-			}
-		]
+			},
+		],
+	},
+
+	resolve: {
+		extensions: [".scss", ".tsx", ".ts", ".js"],
+	},
+	
+	output: {
+		filename: "js/[name].js",
+		path: cd + "/dist",
 	},
 
 	plugins: [
-		new MiniCssExtractPlugin(
-			{filename: '[name]/[name].css'}
-		)
-	]
-};
+		new CleanTerminalPlugin(),
+	],
+
+	experiments: {
+		topLevelAwait: true
+	},
+}
