87b78fa1de4db5312a4ada2326dfdebf6e89022f
Cherry <cherry@sparklet.org>
2024-07-19 20:43:38 -0400

    update build script

--- zip.sh
+++ zip.sh
@@ -1,1 +1,2 @@
-(cd dist && rm -f *.spark.zip && for file in *; do zip -r -o ${file}.spark.zip $file; done)
+npx webpack
+(cd dist && rm -f ./*.spark.zip && for file in *; do zip -r -o "${file}.spark.zip" "$file"; done)
